0caef047173edb08d61bd8c8d9beabd054afbaa2
KARTIK1111 <kartikshrikhande11@gmail.com>
2023-03-02 23:59:39 +0530

    e-commerce-website

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const route = require('./src/routes/routes')
+const multer = require("multer")
+
+const app = express()
+app.use(express.json())
+app.use(multer().any())
+
+
+mongoose.connect("mongodb+srv://insh007:Inshad123@firstcluster.p0r04o1.mongodb.net/group25Database",mongoose.set('strictQuery', false))
+.then(()=>console.log("MongoDB is connected"))
+.catch((err)=>console.log(err))
+
+app.use('/',route)
+app.use('/*', async function(req,res){
+    return res.status(400).send("Provided route url is wrong")
+})
+
+app.listen(3000, function(){
+    console.log("Express app is running on port:",3000)
+})
